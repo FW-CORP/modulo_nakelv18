f8633163d8736521d234dde67b05977b5669c192
intaky-dev <intaky@Intaky>
2025-05-23 17:03:43 -0300

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Modulo Nakel',
+    'version': '1.0',
+    'category': 'Fleet',
+    'summary': 'Modificaciones visuales y funcionales para la empresa de distribucion Nakel S.A.',
+    'description': """
+        todo
+    """,
+    'depends': [
+        'base'
+    ],
+    'data': [
+
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
