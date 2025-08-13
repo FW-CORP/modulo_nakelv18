f031e5ed692c8ee43d9d5a4b2201f2ac82dcb303
intaky-dev <kazapeke@gmail.com>
2025-08-13 19:35:43 -0300

    tuki

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Etiquetas de Stock (ES-AR) – vistas",
     "version": "18.0.2.0.2",
-    "summary": "Reemplaza visualmente 'On Hand/A la mano' → 'Stock' y 'Forecasted/Pronosticado' → 'Ingreso' (solo español)",
+    "summary": "Reemplaza visualmente 'On Hand/A la mano' → 'Stock' y 'Forecasted/Pronosticado' → 'Ingreso' en las vistas de inventario.",
     "author": "Intaky Dev",
     "license": "AGPL-3",
     "category": "Localization",
@@ -9,12 +9,9 @@
 	"stock", 
 	"product",
 	"account",
-	"l10n_ar"
      ],
     "data": [
         "views/stock_views.xml",
-	"data/taxes.xml",
-	"data/tax_groups.xml",
     ],
     "installable": True,
 }
--- models/product_category.py
+++ models/product_category.py
@@ -1,11 +1,8 @@
 from odoo import models, fields
 
 class ProductCategory(models.Model):
-    _inherit = "product.category"
-
-    internal_tax_id = fields.Many2one(
-        "account.tax",
-        string="Impuesto interno por defecto",
-        domain=[("tax_group_id.name", "=", "Impuestos Internos")],
-        help="Alicuota interna que se aplicará a los productos de esta categoría si no tienen impuestos definidos."
-    )
+	_inherit = "product.category"
+	internal_tax_id = fields.Many2one(
+    	"account.tax",
+    	domain=[("tax_group_id.name", "=", "Impuestos Internos")],
+)
