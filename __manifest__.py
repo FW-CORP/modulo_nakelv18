bb3a809a4be406c79a599f8e999c078bcaa5f86f
intaky-dev <kazapeke@gmail.com>
2025-08-13 21:49:26 -0300

    new manifest

--- __manifest__.py
+++ __manifest__.py
@@ -1,21 +1,18 @@
 {
-    "name": "Etiquetas de Stock (ES-AR) – vistas",
-    "version": "18.0.2.0.2",
-    "summary": "Reemplaza visualmente 'On Hand/A la mano' → 'Stock' y 'Forecasted/Pronosticado' → 'Ingreso' en las vistas de inventario.",
+    "name": "Etiquetas POS y Stock ES",
+    "version": "18.0.1.0.0",
     "author": "Intaky Dev",
     "license": "AGPL-3",
     "category": "Localization",
     "depends": [
-        "stock", 
+        "stock",
         "product",
-        "account",
-        "web",
         "point_of_sale",
-        "l10n_ar_pos",
+        "l10n_ar_pos"
     ],
     "data": [
         "views/stock_views.xml",
         "views/modify_pos.xml"
     ],
-    "installable": True,
+    "installable": True
 }
