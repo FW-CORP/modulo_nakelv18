9a50f1449726edade6dcc061022a755a879d9da2
intaky-dev <kazapeke@gmail.com>
2025-08-13 22:33:00 -0300

    17

--- __manifest__.py
+++ __manifest__.py
@@ -1,22 +1,10 @@
 {
-    "name": "Etiquetas POS y Stock ES",
+    "name": "Cambio Texto Logout POS",
     "version": "18.0.1.0.0",
-    "author": "Intaky Dev",
-    "license": "AGPL-3",
-    "category": "Localization",
-    "depends": [
-        "stock",
-        "product",
-        "point_of_sale",
-        "l10n_ar_pos"
-    ],
-    "data": [
-        "views/stock_views.xml",
-        "views/pos_templates.xml"
-    ],
+    "depends": ["point_of_sale"],
     "assets": {
-        "point_of_sale_assets": [
-            "modulo_nakelv18/static/src/js/pos_custom.js",
+        "point_of_sale.assets": [
+            "custom_pos_text/static/src/js/pos_custom.js",
         ]
     },
     "installable": True
